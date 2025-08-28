3f48c1e140088bd738b0e8e25448db2d0d159452
Z4M55 <samuel.serna119@gmail.com>
2025-08-28 09:57:53 -0500

    Update intro.py

--- intro.py
+++ intro.py
@@ -1,3 +1,10 @@
 import streamlit as st #traer objeto de libreria "st"
+from PIL import Image
 
 st.title("Mi primeera APP!")
+
+st.header("En este espacio comienzo a desarrollar ...")
+st.write("Faclmente puedo realizar backend y frontend.")
+image=Image.open('BTM.jpg')
+
+st.image(image, caption='Interfaces multimodales´)
